2a5f4b8f3ba372b3ea6ee7099fc25967021581bf
Sebastian Preciado Aguilar <105876146+GaLGoZo@users.noreply.github.com>
2023-11-22 12:26:11 -0800

    Se adjuntan los documentos del proyecto por el nuevo repositorio

---
+++ SistemaPuntoVenta_Venecia/SQLQuery2.sql
@@ -0,0 +1,11 @@
+select * from usuario
+
+select * from rol
+
+insert into rol (Descripcion)
+values('ADMINISTRADOR')
+
+insert into USUARIO(Documento,NombreCompleto,Correo,Clave,IdRol,Estado)
+values
+
+('101010','ADMIN','@GMAIL','123',1,1)
